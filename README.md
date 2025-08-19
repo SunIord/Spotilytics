ad09708f6c86ce35d604a6a4f46f9e9df190a6d2
jpam <160938086+SunIord@users.noreply.github.com>
2025-08-19 13:33:38 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # Spotilytics
+**Spotilytics** é uma aplicação web interativa feita com Streamlit que consome dados da API do Spotify para apresentar visualizações e insights sobre músicas, artistas ou playlists.
+
+## Como rodar o projeto
+Siga os passos abaixo para rodar a aplicação localmente:
+
+### 1. Clone o repositório
+```bash
+git clone https://github.com/SunIord/Spotilytics
+cd Spotilytics
+```
+
+### 2. Crie um ambiente virtual
+```bash
+python -m venv .venv
+```
+
+### 3. Ative o ambiente
+#### Windows (cmd):
+```bash
+.venv\Scripts\activate
+```
+#### PowerShell:
+```bash
+.\.venv\Scripts\Activate.ps1
+```
+#### Linux/macOS:
+```bash
+source .venv/bin/activate
+```
+
+### 4. Instale as dependências
+```bash
+pip install -r requirements.txt
+```
+
+### 5. Configure as variáveis de ambiente
+#### Crie um arquivo `.env` na raiz do projeto e adicione suas credenciais da API do Spotify:
+```ini
+spotipyId=SEU_CLIENT_ID
+spotipySecret=SEU_CLIENT_SECRET
+``` 
+#### Você pode obter essas credenciais em: https://developer.spotify.com
+
+### 6. Execução
+```bash
+streamlit run app.py
+```
---
+++ Spotilytics/app.py
@@ -0,0 +1,36 @@
+import streamlit as st # used to create the web interface
+import pandas as pd # used for data manipulation
+import plotly.express as px # used to create interactive charts
+import spotipy # used to interact with the Spotify API
+from spotipy.oauth2 import SpotifyClientCredentials
+import os # used to access environment variables
+from dotenv import load_dotenv  
+
+# Authentication with the Spotify API using environment variables
+load_dotenv()
+
+client_id = os.getenv("spotipyId")
+client_secret = os.getenv("spotipySecret")
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+    client_id=client_id,
+    client_secret=client_secret
+))
+
+# Page configuration
+st.set_page_config(page_title="Spotilytics", page_icon="🎶", layout="wide")
+st.title("Spotilytics")
+
+# Search for the artist and display their information
+artistName = st.text_input("Type the name of an artist:")
+
+if artistName:
+    results = sp.search(q='artist:' + artistName, type='artist')
+    if results['artists']['items']:
+        artist = results['artists']['items'][0]
+        st.image(artist['images'][0]['url'], width=200)
+        st.write(f"**Name:** {artist['name']}")
+        st.write(f"**Followers:** {artist['followers']['total']}")
+        st.write(f"**Popularity:** {artist['popularity']}")
+    else:
+        st.write("Artist not found.")
